f247ec438cd8d1b285d823034723c72a608c0b7c
José Luis Marrugo Madrid <61395538+jlmarrugom@users.noreply.github.com>
2023-05-06 21:24:41 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from streamlit_cropper import st_cropper
+from methods import get_final_time, get_random_img,compute_text_tokenization, compute_matching_probs
+import numpy as np
+import datetime
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+st.title("Welcome to the Spot cropper app")
+
+label_list=["a cat", "a bunny","a penguin", "a dog","an owl", 
+            "an owl with a bow tie", "a house", "a person", "a raccoon", "junk"]
+
+text_tokens = compute_text_tokenization(label_list)
+
+img, label = get_random_img()
+
+if st.checkbox("Start Game!"):
+    col1, col2 = st.columns([3,1])
+    game_placeholder = col1.empty()
+    #game_container = game_placeholder.container()
+    now = datetime.datetime.now()
+    #Wit cache resource, it is called just one time per session
+    final_hour = get_final_time(total_seconds = 30)
+    with game_placeholder.container():
+
+        # Get a cropped image from the frontend
+        cropped_img = st_cropper(img, realtime_update=True, box_color='#0000FF',
+                                    aspect_ratio=(1, 1))
+    col2.write(f"Find the {label}")
+    time_place = col2.empty()
+    preview_text = col2.empty()
+    preview_img =  col2.empty()
+    final_label = col2.empty()
+    success = False
+
+    while now <= final_hour:
+        time_place.header(str(round((final_hour-now).total_seconds())))
+
+        # Manipulate cropped image at will
+        preview_text.write("Preview")
+            
+        _ = cropped_img.thumbnail((150,150))
+        preview_img.image(cropped_img)
+
+        match_probs = compute_matching_probs(cropped_img, text_tokens)
+
+        result = label_list[np.argmax(match_probs)]
+        final_label.write(result)
+        if result ==f"a {label}":
+            success = True
+            break
+
+        now = datetime.datetime.now()
+
+    get_final_time.clear()
+    get_random_img.clear()
+
+    if success:
+        game_placeholder.success(f"You found the {label}!")
+    else:
+        game_placeholder.error(f"You didn't found the {label}")
---
+++ methods.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import clip
+import torch
+import datetime
+from random import random
+from PIL import Image
+import os
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+@st.cache_resource
+def get_final_time(total_seconds = 10):
+    delta = datetime.timedelta(0,total_seconds)
+    now = datetime.datetime.now()
+
+    final_time = now + delta
+    return final_time
+
+@st.cache_resource
+def get_random_img():
+    # Upload an image and set some options for demo purposes
+    assets_dir = "./assets/"
+    img_files = os.listdir(assets_dir)
+    img_file = os.path.join(assets_dir,img_files[round(random()*(len(img_files)-1))])
+    label_str = img_file.split("_")[-1].split(".")[0]
+    if label_str=="bunny":
+        label="bunny"
+    else:
+        label="cat"
+    img = Image.open(img_file)
+    return img, label
+
+@st.cache_resource
+def load_clip_model():
+    model, preprocess = clip.load("ViT-B/32", device=device)
+    return model, preprocess
+
+@st.cache_data
+def compute_text_tokenization(label_list):
+    text = clip.tokenize(label_list).to(device)
+
+    return text
+
+def compute_matching_probs(cropped_img, text_tokens):
+    model, preprocess = load_clip_model()
+    image = preprocess(cropped_img).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        logits_per_image, _ = model(image, text_tokens)
+        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    
+    return probs
